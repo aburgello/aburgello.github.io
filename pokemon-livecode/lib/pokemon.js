213302a96f1b769022c7fa2a244e923c36dc3601
Antonio Burgello <burgello.antonio@gmail.com>
2024-09-18 19:49:31 +0100

    Update pokemon.js

--- pokemon-livecode/lib/pokemon.js
+++ pokemon-livecode/lib/pokemon.js
@@ -5,7 +5,7 @@ const resultMessage = document.getElementById('result-message');
 
 async function fetchPokemonData() {
   try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
     const data = await response.json();
     const pokemon = data.results;
 
